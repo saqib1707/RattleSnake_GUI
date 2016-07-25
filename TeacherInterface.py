372c0058aebefefa4872c81a41f37e2d580cced9
saqib1707 <azimsaqib10@gmail.com>
2016-07-25 05:04:30 -0400

    A Teacher Interface for a teacher to login and further can see the list of students in his/her batch,as well as modify marks of an individual

--- TeacherInterface.py
+++ TeacherInterface.py
@@ -1,28 +1,28 @@
-import json,Error,getpass,os
+import json,os
 import tkMessageBox
 from Tkinter import *	
 		
 class Login:
 	def __init__(self):		
-		print
+		print ""
 
 	def checkBatchEmpty(self,batch):
 		with open('StudentRecords.txt') as readStudentFile:
-			jsonData2=json.load(readStudentFile)
-			for i in range(len(jsonData2)):
-				if jsonData2[i]['batch']==batch:
+			jsonData=json.load(readStudentFile)
+			for i in range(len(jsonData)):
+				if jsonData[i]['batch']==batch:
 					return False
 			tkMessageBox.showinfo('Error 404','Batch is empty')
 			return True
 
 	def getMarks(self):
 		with open('StudentRecords.txt') as readStudentFile:
-			jsonData2=json.load(readStudentFile)
-			for i  in range(len(jsonData2)):
-				if jsonData2[i]['name']==self.name_entry.get() and self.marks_entry.get()!="" and jsonData2[i]['batch']==self.batch:
-					jsonData2[i]['marks']=self.marks_entry.get()
+			jsonData=json.load(readStudentFile)
+			for i  in range(len(jsonData)):
+				if jsonData[i]['name']==self.name_entry.get() and self.marks_entry.get()!="" and jsonData[i]['batch']==self.batch:
+					jsonData[i]['marks']=self.marks_entry.get()
 					msg="Marks Modified for %s"%self.name_entry.get()
-					tkMessageBox.showinfo('Congos',msg)
+					tkMessageBox.showinfo('Cngos',msg)
 					self.is_Modified=True
 					break
 			if self.is_Modified==False:
@@ -30,11 +30,11 @@ class Login:
 				self.name_entry.delete(0,'end')
 				self.marks_entry.delete(0,'end')
 				return
-			open('StudentRecords.txt','w').write(json.dumps(jsonData2,indent=4))
+			open('StudentRecords.txt','w').write(json.dumps(jsonData,indent=4))
 			count=0
 			for widget in main.frame.winfo_children():
 				count+=1
-				if count>=10:
+				if count>=11:
 					widget.destroy()
 
 	def modifyMarks(self):
@@ -55,23 +55,23 @@ class Login:
 			return
 		listOfStudent=[]
 		with open('StudentRecords.txt') as readStudentFile:
-			jsonData2=json.load(readStudentFile)
-			for i in range(len(jsonData2)):
-				if jsonData2[i]['batch']==self.batch:
-					data={jsonData2[i]['name']:jsonData2[i]['marks']}
+			jsonData=json.load(readStudentFile)
+			for i in range(len(jsonData)):
+				if jsonData[i]['batch']==self.batch:
+					data={jsonData[i]['name']:jsonData[i]['marks']}
 					listOfStudent.append(data)
-			with open('/home/saqib1707/Desktop/Extra_RattleSnake_copy/temp.txt','w') as tempFile:
+			with open('/home/saqib1707/Desktop/RattleSnake_GUI/temp.txt','w') as tempFile:
 				json.dump(listOfStudent,tempFile,indent=4)
-			os.system('xdg-open /home/saqib1707/Desktop/Extra_RattleSnake_copy/temp.txt')
+			os.system('xdg-open /home/saqib1707/Desktop/RattleSnake_GUI/temp.txt')
 
 	def uploadMarks(self):
 		if self.checkBatchEmpty(self.batch)==True:
 			return
 		with open('StudentRecords.txt') as readStudentFile:
-			jsonData2=json.load(readStudentFile)
+			jsonData=json.load(readStudentFile)
 			for i in range(len(jsonData2)):
-				if jsonData2[i]['batch']==self.batch:
-					arg='Enter marks for '+jsonData2[i]['name']+':'
+				if jsonData[i]['batch']==self.batch:
+					arg='Enter marks for '+jsonData[i]['name']+':'
 					Label(frame,text=arg,bg='#58F0AB',padx='10px',pady='3px').place(x=80,y=50)
 					marks_entry=Entry(frame)
 					marks_entry.place()
@@ -95,32 +95,19 @@ class Login:
 			for i in range(len(jsonData1)):
 				if jsonData1[i]['username']==username and jsonData1[i]['password']==pwd :
 					self.batch=jsonData1[i]['batch']
-					"""
-					text=Text(frame)
-					text.insert(INSERT,jsonData1[i]['name'])
-					text.insert(END,'-Batch ')
-					text.insert(END,batch)
-					text.insert(END,'\nAccess Granted')
-					text.pack()
-					"""
 					msg=str(jsonData1[i]['name'])+' -Batch  '+str(self.batch)+'\nAccess Granted'
 					msg_widget=Message(main.frame,text=msg,width=500,bg='#9FE4F3',padx='10px',pady='5px')
 					msg_widget.place(x=220,y=190)
-					#print "\n",jsonData1[i]['name']," -Batch ",batch,"\n","Access Granted"
 					if jsonData1[i]['is_Marks_Updated']=="False":
-						print "Reminder to upload the latest test marks"
-					#obj=TeacherInterface()
-					showList_btn=Button(main.frame,text='List students in ur Batch',command=self.showListOfStudents,activebackground='grey',activeforeground='#AB78F1',cursor='arrow',bg='#58F0AB',highlightcolor='red',padx='10px',pady='3px').place(x=40,y=260)
-					uploadMarks_btn=Button(main.frame,text='Upload marks of latest test',command=self.uploadMarks,activebackground='grey',activeforeground='#AB78F1',cursor='arrow',bg='#58F0AB',highlightcolor='red',padx='10px',pady='3px').place(x=215,y=260)
-					modifyMarks_btn=Button(main.frame,text='Modify marks of student',command=self.modifyMarks,activebackground='grey',activeforeground='#AB78F1',cursor='arrow',bg='#58F0AB',highlightcolor='red',padx='10px',pady='3px').place(x=400,y=260)
+						tkMessageBox.showinfo('Warning','Reminder to upload the latest test marks')
+					Button(main.frame,text='List students in ur Batch',command=self.showListOfStudents,activebackground='grey',activeforeground='#AB78F1',cursor='arrow',bg='#58F0AB',highlightcolor='red',padx='10px',pady='3px').place(x=40,y=260)
+					Button(main.frame,text='Upload marks of latest test',command=self.uploadMarks,activebackground='grey',activeforeground='#AB78F1',cursor='arrow',bg='#58F0AB',highlightcolor='red',padx='10px',pady='3px').place(x=215,y=260)
+					Button(main.frame,text='Modify marks of student',command=self.modifyMarks,activebackground='grey',activeforeground='#AB78F1',cursor='arrow',bg='#58F0AB',highlightcolor='red',padx='10px',pady='3px').place(x=400,y=260)
 					break
 			if self.batch==None:
 				tkMessageBox.showinfo('Access Denied','Username or Password or both are incorrect!!! Try Again')
 				main.username_entry.delete(0,'end')
 				main.password_entry.delete(0,'end')
-
-	
-
 
 class TeacherInterface:			
 	def __init__(self):
@@ -129,6 +116,10 @@ class TeacherInterface:
 		self.frame.geometry('600x400+350+150')
 		self.frame.config(bg='#ABADAC')
 		self.frame.resizable(width=False,height=False)
+
+	def exit(self):
+		self.frame.destroy()
+
 	def back(self):
 		login_obj=Login()
 		username_label=Label(self.frame,text="User Name",bg='#58F0AB',padx='10px',pady='3px').place(x=80,y=50)
@@ -137,7 +128,9 @@ class TeacherInterface:
 		password_label=Label(self.frame,text="Password",bg='#58F0AB',padx='10px',pady='3px').place(x=310,y=50)
 		self.password_entry=Entry(self.frame,show="*",width=20)
 		self.password_entry.place(x=390,y=50)
-		submit=Button(self.frame,text="Login",command=login_obj.login,activebackground='grey',activeforeground='#AB78F1',cursor='arrow',bg='#58F0AB',highlightcolor='red',padx='10px',pady='3px').place(x=250,y=100)
+		Button(self.frame,text="Login",command=login_obj.login,activebackground='grey',activeforeground='#AB78F1',cursor='arrow',bg='#58F0AB',highlightcolor='red',padx='10px',pady='3px').place(x=250,y=100)
+		Button(self.frame,text='Exit',command=self.exit,activebackground='grey',activeforeground='#AB78F1',bg='#58F0AB',highlightcolor='red',padx='10px',pady='3px').place(relx=0.45,rely=0.35)
+        
 main=TeacherInterface()
 main.back()
 main.frame.mainloop()
